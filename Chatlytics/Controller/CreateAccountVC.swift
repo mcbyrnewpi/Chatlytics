4b08e72bc65269a965855c53b7e753ae548a2720
Matt Byrne <mcbyrnewpi@yahoo.com>
2018-02-16 16:55:46 -0500

    Add create user functionality and segue to channels after completion

--- Chatlytics/Controller/CreateAccountVC.swift
+++ Chatlytics/Controller/CreateAccountVC.swift
@@ -11,11 +11,14 @@ import UIKit
 class CreateAccountVC: UIViewController {
 
     // Outlets
-    
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
+    
+    // Variables
+    var avatarName = "profileDefault" // name of default image
+    var avatarColor = "[0.5, 0.5, 0.5, 1]" // defaults to a light grey color
     
     
     override func viewDidLoad() {
@@ -27,6 +30,7 @@ class CreateAccountVC: UIViewController {
     
     @IBAction func createAccountPressed(_ sender: Any) {
         // create and unwrap variables for registration
+        guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let pass = passTxt.text , passTxt.text != "" else { return }
         
@@ -35,7 +39,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
