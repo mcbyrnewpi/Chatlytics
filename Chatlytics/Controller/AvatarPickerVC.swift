5a90dce1dd804a58910e491f86b396fa30d045bc
Matt Byrne <mcbyrnewpi@yahoo.com>
2018-02-21 13:07:28 -0500

    Add avatar picker view controller and prototype cell

---
+++ Chatlytics/Controller/AvatarPickerVC.swift
@@ -0,0 +1,50 @@
+//
+//  AvatarPickerVC.swift
+//  Chatlytics
+//
+//  Created by Matthew Byrne on 2/21/18.
+//  Copyright © 2018 Matthew Byrne. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    // Outlets
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // this view controller is the object that acts as the delegate of collection view
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            return cell
+        }
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+
+    
+    // Actions
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
+}
---
+++ Chatlytics/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Chatlytics
+//
+//  Created by Matthew Byrne on 2/21/18.
+//  Copyright © 2018 Matthew Byrne. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
