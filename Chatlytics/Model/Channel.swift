e9524ed6cb20bd6d0fce5c9e60b36556e172819c
Matt Byrne <mcbyrnewpi@yahoo.com>
2018-02-27 13:53:25 -0500

    Add get channels function, in addition to Channel model

---
+++ Chatlytics/Model/Channel.swift
@@ -0,0 +1,18 @@
+//
+//  Channel.swift
+//  Chatlytics
+//
+//  Created by Matthew Byrne on 2/27/18.
+//  Copyright © 2018 Matthew Byrne. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+}
+
